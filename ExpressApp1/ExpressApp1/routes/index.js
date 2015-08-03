b242ba3b2ce78eb0749b9437556ca21669930d2b
djonnala <djonnala@sysgain.com>
2015-08-03 14:44:11 -0700

    added slider and retrieved from database

--- ExpressApp1/ExpressApp1/routes/index.js
+++ ExpressApp1/ExpressApp1/routes/index.js
@@ -4,72 +4,54 @@
  */
 var sql = require('mssql');
 var tedious = require('tedious');
+var result = [];
 exports.index = function (req, res) {
     
-    var Connection = require('tedious').Connection;
-    var config = {
-        userName: 'admin',
-        password: 'Sysgain123',
-        server: '168.61.15.99',
-        options: {
-            port: 1433,
-            database: 'AdventureWorks2012',
-            rowCollectionOnDone: true
+    executeStatement();
+    
+    function executeStatement(){
+        var Request = require('tedious').Request;
+        var Connection = require('tedious').Connection;
+        var config = {
+            userName: 'admin',
+            password: 'Sysgain123',
+            server: '168.61.15.99',
+            options: {
+                port: 1433,
+                database: 'AdventureWorks2012',
+                rowCollectionOnDone: true
+            }
         }
-    }
-    var connection = new Connection(config);
-    connection.on('connect', function (err) {
-        console.log("connected");
-        request = new Request("select TOP 4 ProductID from Production.Product;", function (err) {
-            if (err) {
-                console.log(err);
-            }
-        });
-        var result = "";
-        var record = "";
-        request.on('row', function (columns) {
-            columns.forEach(function (column) {
-                if (column.value === null) {
-                    console.log('NULL');
-                } else {
-                    result += column.value + " ";
+        var connection = new Connection(config);
+        connection.on('connect', function (err) {
+            console.log("connected");
+            request = new Request("select TOP 4 ProductID from Production.Product;", function (err) {
+                if (err) {
+                    console.log(err);
                 }
             });
-            console.log(result);
-            // record += result;
-           // result = "";
+            var i = 0;
+            request.on('row', function (columns) {
+                columns.forEach(function (column) {
+                    if (column.value === null) {
+                        console.log('NULL');
+                    } else {
+                        result[i]= column.value;
+                    }
+                    i++;
+                });
+                console.log(result);
+           
+            });
+            connection.execSql(request);
         });
-        connection.execSql(request);
-       //executeStatement();
-            
-        res.render('index', { title: 'Express', year: new Date().getFullYear() , recordset: result });
-    });
-    
-    
-    var Request = require('tedious').Request;
+    }
     var TYPES = require('tedious').TYPES;
-    
-    function executeStatement() {
-        request = new Request("select ProductID from Production.Product;", function (err) {
-            if (err) {
-                console.log(err);
-            }
-        });
-        var result = "";
-        request.on('row', function (columns) {
-            columns.forEach(function (column) {
-                if (column.value === null) {
-                    console.log('NULL');
-                } else {
-                    result += column.value + " ";
-                }
-            });
-            console.log(result);
-            result = "";
-        });
-        connection.execSql(request);
-        
-    }
+    setTimeout(function () {
+        res.render('index', { title: 'Express', year: new Date().getFullYear() , recordset: result });
+    }, 3000);
+   
+ 
 };
 
 exports.about = function (req, res) {
